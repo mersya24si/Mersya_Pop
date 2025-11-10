32e0c461414b31c89b5f71f452818238a1beba0d
Mersya <mersya24si@mahasiswa.pcr.ac.id>
2025-11-10 09:06:02 +0700

    uncomment

--- database/seeders/CreatePelangganDummy.php
+++ database/seeders/CreatePelangganDummy.php
@@ -11,44 +11,44 @@ class CreatePelangganDummy extends Seeder
     /**
      * Run the database seeds.
      */
-    // public function run(): void
-    // {
-    //     $faker = \Faker\Factory::create();
-
-    //     foreach (range(1, 100) as $index) {
-    //         DB::table('pelanggan')->insert([
-    //             'first_name' => $faker->firstName,
-    //             'last_name'  => $faker->lastName,
-    //             'birthday'   => $faker->date('Y-m-d', '2005-12-31'),
-    //             'gender'     => $faker->randomElement(['Male', 'Female', 'Other']),
-    //             'email'      => $faker->unique()->safeEmail,
-    //             'phone'      => $faker->phoneNumber,
-    //         ]);
-    //     }
-    // }
-
-    public function run()
+    public function run(): void
     {
-        $faker = Faker::create('id_ID'); // Menggunakan lokal Indonesia untuk data yang lebih relevan
+        $faker = \Faker\Factory::create();
 
-        $genderOptions = ['Male', 'Female'];
-
-        // Buat 100 data dummy
-        for ($i = 0; $i < 100; $i++) {
-            $gender = $faker->randomElement($genderOptions);
-            $firstName = $faker->firstName($gender);
-            $lastName = $faker->lastName;
-
-            DB::table('pelanggan')->insert([ // Ganti 'pelanggan' jika nama tabel berbeda
-                'first_name' => $firstName,
-                'last_name' => $lastName,
-                'birthday' => $faker->dateTimeBetween('-40 years', '-18 years')->format('Y-m-d'),
-                'gender' => $gender,
-                // Pastikan email unik dan sesuai dengan nama
-                'email' => strtolower($firstName . '.' . $lastName . $faker->unique()->randomNumber(3) . '@' . $faker->safeEmailDomain),
-                // Kolom phone bisa NULL, jadi kita buat probabilitas 80% ada nomor
-                'phone' => $faker->boolean(80) ? $faker->unique()->phoneNumber : NULL,
+        foreach (range(1, 100) as $index) {
+            DB::table('pelanggan')->insert([
+                'first_name' => $faker->firstName,
+                'last_name'  => $faker->lastName,
+                'birthday'   => $faker->date('Y-m-d', '2005-12-31'),
+                'gender'     => $faker->randomElement(['Male', 'Female', 'Other']),
+                'email'      => $faker->unique()->safeEmail,
+                'phone'      => $faker->phoneNumber,
             ]);
         }
     }
+
+    // public function run()
+    // {
+    //     $faker = Faker::create('id_ID'); // Menggunakan lokal Indonesia untuk data yang lebih relevan
+
+    //     $genderOptions = ['Male', 'Female'];
+
+    //     // Buat 100 data dummy
+    //     for ($i = 0; $i < 100; $i++) {
+    //         $gender = $faker->randomElement($genderOptions);
+    //         $firstName = $faker->firstName($gender);
+    //         $lastName = $faker->lastName;
+
+    //         DB::table('pelanggan')->insert([ // Ganti 'pelanggan' jika nama tabel berbeda
+    //             'first_name' => $firstName,
+    //             'last_name' => $lastName,
+    //             'birthday' => $faker->dateTimeBetween('-40 years', '-18 years')->format('Y-m-d'),
+    //             'gender' => $gender,
+    //             // Pastikan email unik dan sesuai dengan nama
+    //             'email' => strtolower($firstName . '.' . $lastName . $faker->unique()->randomNumber(3) . '@' . $faker->safeEmailDomain),
+    //             // Kolom phone bisa NULL, jadi kita buat probabilitas 80% ada nomor
+    //             'phone' => $faker->boolean(80) ? $faker->unique()->phoneNumber : NULL,
+    //         ]);
+    //     }
+    // }
 }
